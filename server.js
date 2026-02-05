cce43aa9587057f5d8126a8f0c4723533e3467db
papneetswain4-alt <papneetswain4@gmail.com>
2026-02-05 17:42:15 +0530

    fix: use dynamic port for production

--- server.js
+++ server.js
@@ -35,7 +35,9 @@ app.use("/api/customers", customerRoutes);
 app.use("/api/menu", menuRoutes);
 
 
-// Server
-app.listen(5000, () => {
-  console.log("🚀 Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
